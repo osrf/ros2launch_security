af7a10316f56eeb7eaf91e272448fa99ebf99047
Geoffrey Biggs <gbiggs@killbots.net>
2021-01-26 10:06:43 +0900

    Add security extensions
    Signed-off-by: Geoffrey Biggs <gbiggs@killbots.net>

---
+++ ros2launch_security/node_action/security.py
@@ -0,0 +1,67 @@
+# Copyright 2021 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pathlib
+from typing import Dict
+from typing import List
+from typing import Union
+
+from launch_ros.actions.node import NodeActionExtension, Node
+
+from launch.launch_context import LaunchContext
+from launch.substitutions import LocalSubstitution
+from launch.utilities import normalize_to_list_of_substitutions
+
+import nodl
+import sros2.api._keystore
+
+
+class SecurityNodeActionExtension(NodeActionExtension):
+    def command_extension(self, context):
+        if context.launch_configurations.get('__secure', None) is not None:
+            cmd_extension = ['--enclave', LocalSubstitution("ros_specific_arguments['enclave']")]
+            cmd_extension = [normalize_to_list_of_substitutions(x) for x in cmd_extension]
+            return cmd_extension
+        else:
+            return []
+
+    def execute(self, context, ros_specific_arguments, node_info):
+        if context.launch_configurations.get('__secure', None) is not None:
+            return self._setup_security(context, ros_specific_arguments, node_info)
+        else:
+            return ros_specific_arguments
+
+    def _setup_security(
+        self,
+        context: LaunchContext,
+        ros_specific_arguments: Dict[str, Union[str, List[str]]],
+        node_info: NodeActionExtension.NodeInfo
+    ) -> None:
+        """Enable encryption, creating a key for the node if necessary."""
+        nodl_node = nodl.get_node_by_executable(
+            package_name=node_info.package,
+            executable_name=node_info.executable
+        )
+
+        self.__enclave = node_info.name.replace(
+            Node.UNSPECIFIED_NODE_NAME, nodl_node.name
+        ).replace(Node.UNSPECIFIED_NODE_NAMESPACE, '')
+
+        sros2.api._keystore.create_enclave(
+            keystore_path=pathlib.Path(context.launch_configurations.get('__keystore')),
+            identity=self.__enclave
+        )
+
+        ros_specific_arguments['enclave'] = self.__enclave
+        return ros_specific_arguments
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'ros2launch_security'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+    ],
+    install_requires=['ros2launch'],
+    zip_safe=True,
+    author='Joe Example',
+    author_email='joe@example.com',
+    maintainer='Joe Example',
+    maintainer_email='joe@example.com',
+    url='https://github.com/ros2/launch/tree/master/ros2launch_security',
+    download_url='https://github.com/ros2/ros2launch_security/releases',
+    keywords=[],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+    ],
+    description='The security extension for ros2launch',
+    long_description="""\
+The package provides an extension adding security capabilities to the ROS 2 launch command line tool.""",
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'ros2launch.option': [
+            'security = ros2launch_security.option.security:SecurityOption',
+        ],
+        'launch_ros.node_action': [
+            'security = ros2launch_security.node_action.security:SecurityNodeActionExtension',
+        ],
+    }
+)
