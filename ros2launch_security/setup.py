1d0db0e93637b95096cc158eb9e5184c20b60c03
William Woodall <william@osrfoundation.org>
2021-08-11 13:55:39 -0700

    1.0.0
    Signed-off-by: William Woodall <william@osrfoundation.org>

--- ros2launch_security/setup.py
+++ ros2launch_security/setup.py
@@ -5,7 +5,7 @@ package_name = 'ros2launch_security'
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version='1.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
